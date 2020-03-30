cd428c4387810cec440e253fbed58e77166f2459
Axelandre42 <5228298+Axelandre42@users.noreply.github.com>
2020-03-30 16:30:11 +0200

    Add code for services.yaml loading in FortressFolkExtension.php

--- src/DependencyInjection/FortressFolkExtension.php
+++ src/DependencyInjection/FortressFolkExtension.php
@@ -4,18 +4,25 @@
 namespace Fortress\Folk\DependencyInjection;
 
 
+use Exception;
 use Fortress\Folk\Security\LoginFormAuthenticator;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class FortressFolkExtension extends Extension
 {
 
 	/**
 	 * @inheritDoc
+	 * @throws Exception
 	 */
 	public function load(array $configs, ContainerBuilder $container)
 	{
+		$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+		$loader->load('services.yaml');
+
 		$configuration = new Configuration();
 		$config = $this->processConfiguration($configuration, $configs);
 

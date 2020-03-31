d7426a746577aea9535c849c3adeb01102ff9450
Alexandre Waeles <5228298+Axelandre42@users.noreply.github.com>
2020-03-31 19:32:49 +0200

    Fix missing submit button

--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -19,6 +20,7 @@ class LoginFormType extends AbstractType
 			->add('username', TextType::class)
 			->add('password', PasswordType::class)
 			->add('remember_me', CheckboxType::class)
-			->add("csrf_token", HiddenType::class);
+			->add("csrf_token", HiddenType::class)
+			->add('submit', SubmitType::class);
 	}
 }
